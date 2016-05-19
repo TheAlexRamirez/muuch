9debbbdbf6297a1a6d1077358941d2e006121163
Alex Ramirez <aleramirez9417@gmail.com>
2016-05-18 19:25:06 -0500

    added video introd

--- public/javascripts/realTimeApp.js
+++ public/javascripts/realTimeApp.js
@@ -103,9 +103,9 @@ function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
     .state('home', {
-      url: '/home',
+      url: '/',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'HomeCtrl'
     });
 
 	$stateProvider
@@ -144,11 +144,89 @@ function($stateProvider, $urlRouterProvider) {
 		  }
 	});
 	
-  //$urlRouterProvider.otherwise('home');
+  $urlRouterProvider.otherwise('/');
 }]);
 
 
 //Controller for projectDetails/{:id}
+app.controller('HomeCtrl', [
+
+'$scope',
+'$stateParams',
+'projects',
+'$state',
+'auth',
+'Message',
+'$firebaseArray',
+'$http',
+function( $scope, $stateParams, projects, $state, auth,Message,$firebaseArray,$http){
+//jQuery is required to run this code
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
+}]);
+
+
+//Controller for /home
 app.controller('detailProjectCtrl', [
 'post',
 '$scope',
@@ -162,6 +240,7 @@ app.controller('detailProjectCtrl', [
 function(post, $scope, $stateParams, projects, $state, auth,Message,$firebaseArray,$http){
 	$scope.projectF = post;
 }]);
+
 
 
 //Controller for user/{:id}
